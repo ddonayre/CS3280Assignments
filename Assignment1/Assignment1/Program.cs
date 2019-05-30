04360b801befdeda5c2c341a31320b4cdc8ef00d
David D <50682968+ddonayre@users.noreply.github.com>
2019-05-29 19:33:37 -0600

    A1wip

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //variables
+            int n1 = 0;
+            int n2 = 0;
+            string input;
+           // int sum;
+           // int sub;
+           // int mult;
+           // int div;
+
+
+            // Getting the numbers
+
+
+            bool result = false;
+            //Convert
+            while (result == false)
+            {
+                Console.Write(" Please enter the first number...:");
+                input = Console.ReadLine();
+                result = int.TryParse(input, out n1);
+                if (result == false)
+                {
+                    Console.WriteLine($"{input} is not a valid int");
+                }
+            }
+            result = false;
+            while (result == false)
+            {
+
+                Console.Write("Please enter the second number...:");
+                input = Console.ReadLine();
+                result = int.TryParse(input, out n2);
+                if (result == false)
+                {
+                    Console.WriteLine($"{input} is not a valid int");
+                }
+            }
+
+            Console.WriteLine($"{n1} + {n2} = {n1 + n2}");  // inside of curly is an expression (string interpolation)
+            Console.WriteLine($"{n1} - {n2} = {n1 - n2}");
+            Console.WriteLine($"{n1} * {n2} = {n1 * n2}");
+            Console.WriteLine($"{n1} / {n2} = {n1 / n2}");
+            //Console.Read();
+
+            // IF statements
+
+            if (n1 < n2)
+            {
+                Console.WriteLine($"{n1} is less than {n2}");
+            }
+            else
+            {
+                Console.WriteLine($"{n1} is not less than {n2}");
+            }
+
+            if (n1 > n2)
+            {
+                Console.WriteLine($"{n1} is greater than {n2}");
+            }
+            else
+            {
+                Console.WriteLine($"{n1} is not greater than {n2}");
+            }
+
+            if (n1 != n2)
+            {
+                Console.WriteLine($"{n1} does not equal {n2}");
+            }
+            else
+            {
+                Console.WriteLine($"{n1} equals {n2} ");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Assignment1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            result = MessageBox.Show($"You Typed: {textBox1.Text}", "MessageBox 1",MessageBoxButtons.RetryCancel, MessageBoxIcon.Asterisk);
+            label4.Text = "You clicked the " + result.ToString() + "button";
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            result = MessageBox.Show($"You Typed: {textBox2.Text}", "MessageBox 2", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            label4.Text = "You clicked the " + result.ToString() + "button";
+        }
+    }
+}
