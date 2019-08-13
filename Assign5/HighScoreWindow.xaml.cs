df080139df6d1e7388301071fbd3a2edfd462a52
David D <50682968+ddonayre@users.noreply.github.com>
2019-08-12 20:26:01 -0600

    assign5isworkingnow

--- Assign5/HighScoreWindow.xaml.cs
+++ Assign5/HighScoreWindow.xaml.cs
@@ -1,16 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Assign5
 {
@@ -20,7 +9,7 @@ namespace Assign5
     public partial class HighScoreWindow : Window
     {
         private User score;
-    
+
         public HighScoreWindow()
         {
             InitializeComponent();
@@ -37,7 +26,7 @@ namespace Assign5
 
         private void Window_ContentRendered(object sender, EventArgs e)
         {
-            
+
         }
     }
 }
